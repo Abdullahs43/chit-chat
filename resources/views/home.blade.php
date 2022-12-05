bb399a67b446159997a1b5442e5f71a62b78a64f
Abdullah43 <abdullah.sharif@devsinc.com>
2022-12-05 12:09:31 +0500

    minor changes implement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="users">
+            <h5>User</h5>
+            <ul class="list-group list-chat-item">
+                <li class="chat-user-list">
+                    <a href="">
+                        Malik
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
