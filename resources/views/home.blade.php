0b623c933f9a359c0f1045bfb43c5b909fa967ac
Abdullah43 <abdullah.sharif@devsinc.com>
2022-12-05 15:09:17 +0500

    Basic layout and Database tables implement: Basic layout implemented Migrations for messages and user messages are created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
+<div class="row chat-row">
+  
     <div class="col-md-3">
         <div class="users">
-            <h5>User</h5>
+            <h5>Users</h5>
             <ul class="list-group list-chat-item">
-                <li class="chat-user-list">
-                    <a href="">
-                        Malik
+                @if($users->count())
+                  @foreach ($users as $user)
+                  <li class="chat-user-list d-flex">
+                    <div class="user-image bg-primary text-light">
+                        <div class="user-image-name"><center>  {{makeImageFromName($user->name)}} </center></div>
+                    </div>
+                    <a href="" class="user-name">
+                        {{$user->name}}
                     </a>
-                </li>
+                  </li>
+                  @endforeach
+                @endif
             </ul>
         </div>
     </div>
