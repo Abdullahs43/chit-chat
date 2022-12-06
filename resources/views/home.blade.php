934237b8ea2afb8d8f1f73db1e16901f9a8dc2e2
Abdullah43 <abdullah.sharif@devsinc.com>
2022-12-06 14:25:08 +0500

    Summernote js editior configure Summernote configured Bugs find and removed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,12 @@
             <ul class="list-group list-chat-item">
                 @if($users->count())
                   @foreach ($users as $user)
-                  <li class="chat-user-list d-flex">
+                  <li class="chat-user-list d-flex {{ $loop->first? "active" : ""; }}">
                     <div class="user-image bg-primary text-light">
                         <div class="user-image-name"><center>  {{makeImageFromName($user->name)}} </center></div>
+                       
                     </div>
+                    <i class="fa-regular fa-circle text-dark status"></i>
                     <a href="" class="user-name">
                         {{$user->name}}
                     </a>
@@ -22,5 +24,47 @@
             </ul>
         </div>
     </div>
+    <div class="col-md-9  conversation-section">
+        <div class="chat-header">
+            <li class="chat-user-list d-flex ">
+                <div class="user-image bg-primary text-light">
+                    <div class="user-image-name"> <center> {{makeImageFromName("Malik Abdullah")}} </center> </div>
+                   
+                </div>
+                <a href="" class="user-name-head">
+                    Malik Abdullah
+                </a>
+                <i class="fa-regular fa-circle text-dark  user-status-head" title="away"></i>
+              </li>
+        </div>
+        <div class="converation-message">
+            <div class="chat-header">
+                <li class="chat-user-list d-flex ">
+                    <div class="user-image bg-primary text-light">
+                        <div class="user-image-name"> <center> {{makeImageFromName("Malik Abdullah")}} </center> </div>
+                       
+                    </div>
+                    <a href="" class="user-in-coversation">
+                        Malik Abdullah
+                    </a>
+                    <span class="time text-gray-500" >10:20 PM</span>
+                  </li>
+            </div>
+            <div class="message">
+                hellowewe qweqw
+            </div>
+        </div>
+        <div class="input">
+            <div id="input-field"></div>
+        </div>
+       
+    </div>
+    
+  
 </div>
+@endsection
+@section('script')
+<script>
+    $('#input-field').summernote({ });
+  </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,16 @@
     <title>Chit Chat Application</title>
 
     <!-- Fonts -->
+   
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/style.css'])
+    <script src="https://kit.fontawesome.com/eb5ef8a182.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 </head>
 <body>
     <div id="app">
@@ -77,4 +82,5 @@
         </main>
     </div>
 </body>
+@yield('script')
 </html>
